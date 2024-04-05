eae10fad5ab7c95dfb46ad06d242d0e3a96df880
shornada <adam.shorny@seznam.cz>
2024-04-05 16:15:23 +0200

    selected part in sidebar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -7,94 +7,98 @@ import { GrNodes } from "react-icons/gr";
 
 
 import {
-	BsArrowRight,
-	BsArrowLeft,
+  BsArrowRight,
+  BsArrowLeft,
 } from "react-icons/bs";
 import { IoGrid } from "react-icons/io5";
+import { CurrentView } from "../App";
 
 
 
 interface SideBarProps {
-    toggleView: (view: 'graph' | 'grid'|'help') => void;
-  }
-  
-  export function SideBar({ toggleView }: SideBarProps) {
-    const [sideBar, setSideBar] = useState(false);
-  
-    const handleChangeSideBar = () => {
-      setSideBar(!sideBar);
-    };
-  
-    const handleViewChange = (view: 'graph' | 'grid'|'help') => {
-      toggleView(view);
-      setSideBar(false); // Close the sidebar after changing the view
-    };
-  
-    return (
-      <Container>
-        <Content>
-          {!sideBar ? (
-            <ClosedSideBar>
+  toggleView: (view: 'graph' | 'grid' | 'help') => void;
+}
+
+export function SideBar({ toggleView }: SideBarProps) {
+  const [sideBar, setSideBar] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<CurrentView>('graph'); // State to store the selected item
+
+
+  const handleChangeSideBar = () => {
+    setSideBar(!sideBar);
+};
+
+const handleViewChange = (view: CurrentView) => {
+    toggleView(view);
+    setSelectedItem(view); // Update the selected item when changing views
+    setSideBar(false); // Close the sidebar after changing the view
+};
+
+  return (
+    <Container>
+      <Content>
+        {!sideBar ? (
+          <ClosedSideBar>
+            <nav>
+              <button onClick={handleChangeSideBar}>
+                <BsArrowRight />
+              </button>
+              {/* Links principais do app */}
+              <ul>
+                <li>
+                <a href="#" title="Graf" onClick={() => handleViewChange('graph')} className={selectedItem === 'graph' ? 'selected' : ''}>
+                    <GrNodes />
+                  </a>
+                </li>
+                <li>
+                <a href="#" title="Dlaždice" onClick={() => handleViewChange('grid')} className={selectedItem === 'grid' ? 'selected' : ''}>
+                    <IoGrid />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Nápověda" onClick={() => handleViewChange('help')} className={selectedItem === 'help' ? 'selected' : ''}>
+                    <MdOutlineHelp />
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </ClosedSideBar>
+        ) : (
+          <OpenSideBar>
+            <section>
               <nav>
-                <button onClick={handleChangeSideBar}>
-                  <BsArrowRight />
-                </button>
-                {/* Links principais do app */}
+                <span>
+                  <button onClick={handleChangeSideBar}>
+                    <BsArrowLeft />
+                  </button>
+                </span>
+                {/* Icones principais do app */}
                 <ul>
                   <li>
-                    <a href="#" title="Graf" onClick={() => handleViewChange('graph')}>
-                    <GrNodes />
+                  <a href="#" title="Graf" onClick={() => handleViewChange('graph')} className={selectedItem === 'graph' ? 'selected' : ''}>
+                      <GrNodes />
+                      <p>Graf</p>
                     </a>
                   </li>
                   <li>
-                    <a href="#" title="Dlaždice" onClick={() => handleViewChange('grid')}>
-                    <IoGrid />
+                  <a href="#" title="Dlaždice" onClick={() => handleViewChange('grid')} className={selectedItem === 'grid' ? 'selected' : ''}>
+                      <IoGrid />
+                      <p>Dlaždice</p>
                     </a>
                   </li>
                   <li>
-                    <a href="#" title="Nápověda" onClick={() => handleViewChange('help')}>
-                    <MdOutlineHelp />
+                  <a href="#" title="Nápověda" onClick={() => handleViewChange('help')} className={selectedItem === 'help' ? 'selected' : ''}>
+                      <MdOutlineHelp />
+                      <p>Nápověda</p>
                     </a>
                   </li>
                 </ul>
               </nav>
-            </ClosedSideBar>
-          ) : (
-            <OpenSideBar>
-              <section>
-                <nav>
-                  <span>
-                    <button onClick={handleChangeSideBar}>
-                      <BsArrowLeft />
-                    </button>
-                  </span>
-                  {/* Icones principais do app */}
-                  <ul>
-                    <li>
-                      <a href="#" title="Graf" onClick={() => handleViewChange('graph')}>
-                      <GrNodes />
-                        <p>Graf</p>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" title="Dlaždice" onClick={() => handleViewChange('grid')}>
-                      <IoGrid />
-                        <p>Dlaždice</p>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" title="Nápověda" onClick={() => handleViewChange('help')}>
-                      <MdOutlineHelp />
-                       <p>Nápověda</p>
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-              </section>
-              <aside onClick={handleChangeSideBar} />
-            </OpenSideBar>
-          )}
-        </Content>
-      </Container>
-    );
-  }
+            </section>
+            <aside onClick={handleChangeSideBar} />
+          </OpenSideBar>
+        )}
+      </Content>
+    </Container>
+  );
+}
